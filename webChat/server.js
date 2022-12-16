169cbe6cf4d54345e891d64976db3614571820d9
StormyFrolic <stormethan10@gmail.com>
2022-12-15 16:28:27 -0800

    updated for deployment

--- webChat/server.js
+++ webChat/server.js
@@ -1,5 +1,13 @@
 const express = require('express')
 const app = express()
+
+if (process.env.NODE_ENV === 'production') {
+    //*Set static folder up in production
+    app.use(express.static('client/build'));
+
+    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
+  }
+
 const port = 5000
 
 const mongoose = require('mongoose')
