40835f1600b0e115d0260463dc0bf0f4d98eeb8d
StormyFrolic <stormethan10@gmail.com>
2022-12-15 17:17:25 -0800

    update proxy

--- webChat/client/vite.config.js
+++ webChat/client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "http://frolic-webchat.onrender.com:5000",
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, '')
